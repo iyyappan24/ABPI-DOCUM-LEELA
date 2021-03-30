ee6d079f3c68a7db48264e4e5f73417f6d3aebbc
unknown <81483502+iyyappan24@users.noreply.github.com>
2021-03-30 18:43:06 +0530

    modified admin.java

--- Hospital_Management_System/src/com/cg/bookmydoctor/entities/Admin.java
+++ Hospital_Management_System/src/com/cg/bookmydoctor/entities/Admin.java
@@ -9,7 +9,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-@Entity
+
 @Table(name="Admin")
 public class Admin implements Serializable {
 	@Id
